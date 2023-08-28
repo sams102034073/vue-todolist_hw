66ce547970dfaeee84e3a0d82668e95e9a7e81a3
Eddie Kao <eddie@5xcampus.com>
2023-08-28 20:03:21 +0800

    add form style

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,20 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <main class="container mx-auto">
+    <header class="m-2">
+      <h1 class="text-6xl font-thin select-none">TODO!</h1>
+      <div class="font-semibold select-none text-neutral-600">simple and studid todo app</div>
+    </header>
+    <form class="px-10 py-12 bg-white shadow-sm">
+      <section class="flex">
+        <input type="text" placeholder="做點重要的事吧..." class="w-full text-2xl focus:outline-none input-lg input input-bordered" />
+        <button class="text-xl btn-lg btn btn-neutral">新增</button>
+      </section>
+    </form>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
+    <section class="bg-white"></section>
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
