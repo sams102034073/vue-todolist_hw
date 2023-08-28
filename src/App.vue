97ca129ab15b201830c32de90725d3c363c5bd81
Eddie Kao <eddie@5xcampus.com>
2023-08-28 20:41:16 +0800

    add todo task

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,5 @@
 <script setup></script>
 
-<template>
-  <main class="container mx-auto">
-    <header class="m-2">
-      <h1 class="text-6xl font-thin select-none">TODO!</h1>
-      <div class="font-semibold select-none text-neutral-600">simple and studid todo app</div>
-    </header>
-    <form class="px-10 py-12 bg-white shadow-sm">
-      <section class="flex">
-        <input type="text" placeholder="做點重要的事吧..." class="w-full text-2xl focus:outline-none input-lg input input-bordered" />
-        <button class="text-xl btn-lg btn btn-neutral">新增</button>
-      </section>
-    </form>
-
-    <section class="bg-white"></section>
-  </main>
-</template>
+<template></template>
 
 <style scoped></style>
